82161dd4e3abb609cf308899a5c3357f4c5c56e6
Marwa <marwamubarak6273@gmail.com>
2026-06-09 13:47:42 +0300

    fix: add blog-frontend as regular folder

---
+++ blog-frontend/src/app/post-list/post-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  posts: any[] = [];
+  selectedPost: any = null;
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.postService.getPosts().subscribe((data) => {
+      this.posts = data;
+    });
+  }
+
+  showDescription(post: any): void {
+    this.selectedPost = post;
+  }
+}
